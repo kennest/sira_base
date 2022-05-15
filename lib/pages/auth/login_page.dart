0a9a27b73533dbb25fb6fe72c4cfbfadedf25d00
Kennest <kennyoulai@gmail.com>
2022-05-15 22:24:06 +0000

    Login Page redesigned

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:get/get.dart';
 import 'package:get_it/get_it.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:sira_base/services/auth_service.dart';
 import 'package:sira_base/utilities/logger.dart';
 
@@ -36,19 +37,33 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("SIRA CONNECTION HUB"),
+          title: Text(
+            "SIRA CONNECTION HUB",
+            style:
+                GoogleFonts.aBeeZee(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
           backgroundColor: Colors.lightGreen,
         ),
         body: Stack(
           alignment: Alignment.center,
           children: [
             Positioned(
+                top: MediaQuery.of(context).size.height / 6,
+                left: MediaQuery.of(context).size.width / 2.1,
+                child: Align(
+                    alignment: Alignment.center,
+                    child: Icon(
+                      Icons.lock,
+                      size: 80,
+                      color: Colors.grey,
+                    ))),
+            Positioned(
                 top: MediaQuery.of(context).size.height / 3.5,
                 left: MediaQuery.of(context).size.width / 3,
                 child: Align(
                   alignment: Alignment.center,
                   child: LimitedBox(
-                    maxHeight: MediaQuery.of(context).size.height / 2.5,
+                    maxHeight: MediaQuery.of(context).size.height / 3,
                     maxWidth: MediaQuery.of(context).size.width / 3,
                     child: Card(
                       child: Padding(
@@ -83,8 +98,10 @@ class _LoginPageState extends State<LoginPage> {
                                             .secondary,
                                         child: Text(
                                           "Connexion".toUpperCase(),
-                                          style: const TextStyle(
-                                              color: Colors.white),
+                                          style: GoogleFonts.aBeeZee(
+                                              fontSize: 15,
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold),
                                         ),
                                         onPressed: () async {
                                           _formKey.currentState!.save();
